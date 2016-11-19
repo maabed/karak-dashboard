75075d8d61d4f108e086cbb808974a89bb1a2e58
M.Abed <maabed@users.noreply.github.com>
2016-11-19 13:44:42 +0200

    Layout/calender (#13)
    * calender and Map layouts and the components in Dashboard layout
    
    * navegation links and sublinks
    
    * login layout
    
    * login page changes
    
    * rigistration page
    
    * registration and login page
    
    * map and google map layouts
    
    * some changes in map layout
    
    * rename style.css

---
+++ assets/js/map.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=world-population-density.json&callback=?', function (data) {
+
+        // Initiate the chart
+        $('#container').highcharts('Map', {
+
+            title: {
+                text: ''
+            },
+
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+
+            colorAxis: {
+                min: 1,
+                max: 1000,
+                type: 'logarithmic'
+            },
+
+            series: [{
+                data: data,
+                mapData: Highcharts.maps['custom/world'],
+                joinBy: ['iso-a2', 'code'],
+                name: 'Cumulative Sales',
+                states: {
+                    hover: {
+                        color: '#BADA55'
+                    }
+                },
+                tooltip: {
+                    valueSuffix: '0$'
+                }
+            }]
+        });
+    });
+});
