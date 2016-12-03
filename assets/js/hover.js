c26aa7243d22a7cb395fb8407cafe7162f91e3f0
M.Abed <maabed@users.noreply.github.com>
2016-12-03 11:18:43 +0200

    Forms wizard/ buttons/ tabs (#23)
    * Wizard page finish
    
    * wizard changes
    
    * UI Elements, icons page
    
    * icons page
    
    * start working in taps page and some changes in other pages
    
    * some changes
    
    * some changes
    
    * taps page
    
    * tabs page and wizard changes
    
    * form-wizard finishing
    
    * buttons page
    
    * finishing buttons page and hover page
    
    * rename tabs.html
    
    * basic table page
    
    * basic table

---
+++ assets/js/hover.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Used to demonstrate Hover.css only. Not required when adding
+ * Hover.css to your own pages. Prevents a link from being
+ * navigated and gaining focus.
+ */
+var effects = document.querySelectorAll('.effects')[0];
+
+effects.addEventListener('click', function (e) {
+
+    if (e.target.className.indexOf('hvr') > -1) {
+        e.preventDefault();
+        e.target.blur();
+
+    }
+});
---
+++ assets/js/toggle.js
@@ -0,0 +1,24 @@
+﻿$('.single-tap-details').hide();
+$('.toggle-icon').hide();
+$('.single-tap-title').click(function (e) {
+
+    e.preventDefault();
+    // hide all span
+    var $this = $(this).parent().find('.single-tap-details');
+    $(".single-tap-details").not($this).hide(300);
+    // here is what I want to do
+    $this.toggle(300);
+
+});
+
+$('.single-tap-title').click(function (e) {
+
+    e.preventDefault();
+    // hide all span
+    var $this = $(this).parent().find('.toggle-icon2');
+    $(".toggle-icon2").not($this).hide(300);
+    // here is what I want to do
+    $this.toggle(300);
+
+});
+
