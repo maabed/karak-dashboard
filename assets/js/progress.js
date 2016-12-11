6393da223718d043d22e1a59914b6defbe011189
RayanGT <RayanGT>
2016-12-11 15:14:09 +0200

    added materialize.scss

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -267,60 +267,13 @@ $("#semi-progress-container").append("<div id='txt' class='center-block' style='
         /*Spinners colors*/
 
         /*bar spinner*/
-        var opts = {
-        lines: 13 // The number of lines to draw
-                , length: 28 // The length of each line
-                , width: 14 // The line thickness
-                , radius: 42 // The radius of the inner circle
-                , scale: 0.25 // Scales overall size of the spinner
-                , corners: 1 // Corner roundness (0..1)
-                , color: '#000' // #rgb or #rrggbb or array of colors
-                , opacity: 0.25 // Opacity of the lines
-                , rotate: 0 // The rotation offset
-                , direction: 1 // 1: clockwise, -1: counterclockwise
-                , speed: 1 // Rounds per second
-                , trail: 60 // Afterglow percentage
-                , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-                , zIndex: 2e9 // The z-index (defaults to 2000000000)
-                , className: 'spinner' // The CSS class to assign to the spinner
-                , top: '38%' // Top position relative to parent
-                , left: '50%' // Left position relative to parent
-                , shadow: false // Whether to render a shadow
-                , hwaccel: false // Whether to use hardware acceleration
-                , position: 'absolute' // Element positioning
-        }
-var target = document.getElementById('basicspinner')
         $("#spinner").click(function(){
-$("#overlay").addClass("loading-opacity");
-        var spinner = new Spinner(opts).spin(target);
+            $("#overlay").addClass("loading-opacity");
         setTimeout(function(){
-        spinner.stop(); // To stop the spinner
-                $("#overlay").removeClass("loading-opacity");
+            $("#overlay").removeClass("loading-opacity");
         }, 4500);
         });
         /*dots spinners*/
-        var dots = {
-        lines: 13 // The number of lines to draw
-                , length: 0 // The length of each line
-                , width: 14 // The line thickness
-                , radius: 42 // The radius of the inner circle
-                , scale: 0.25 // Scales overall size of the spinner
-                , corners: 1 // Corner roundness (0..1)
-                , color: '#3b97d2' // #rgb or #rrggbb or array of colors
-                , opacity: 0 // Opacity of the lines
-                , rotate: 0 // The rotation offset
-                , direction: 1 // 1: clockwise, -1: counterclockwise
-                , speed: 1 // Rounds per second
-                , trail: 60 // Afterglow percentage
-                , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-                , zIndex: 2e9 // The z-index (defaults to 2000000000)
-                , className: 'spinner' // The CSS class to assign to the spinner
-                , top: '38%' // Top position relative to parent
-                , left: '50%' // Left position relative to parent
-                , shadow: false // Whether to render a shadow
-                , hwaccel: false // Whether to use hardware acceleration
-                , position: 'absolute' // Element positioning
-        }
 var targetdots = document.getElementById('basicspinner-dots')
         $("#spinner-dot").click(function(){
 $("#dots-overlay").addClass("loading-opacity");
