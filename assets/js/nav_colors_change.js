e80ee3664ba114a74726141a0174a9c50fe1f3cb
Meshaal <mohammadz.meshaal@gmail.com>
2016-12-19 15:41:45 +0200

    menue colors finishing

---
+++ assets/js/nav_colors_change.js
@@ -0,0 +1,88 @@
+﻿$(document).ready(function () {
+    $("#preview1").click(function () {
+        $("#head").css("background-color", "#3B97D2");
+        $("#searchTB ").css("background-color", "#3B97D2");
+        $(".sidebar-nav > .sidebar-brand").css("background-color", "#2980B9");
+        $(".badge-notify").css("color", "#3B97D2");
+        $("#sidebar-wrapper").css("background-color", "#fff");
+        $(".sidebar-nav li a").css("border-bottom", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("border-right", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("color", "#34495E");
+        $(".nav-icon").css("color", "#C6C4DB")
+        $(".user-box p").css("color", "#fff");
+        $(".head-box i").css("color", "#fff");
+        $(".sidebar-nav > .sidebar-brand").css("color", "#fff");
+    });
+    $("#preview2").click(function () {
+        $("#head").css("background-color", "#f5d34b");
+        $("#searchTB ").css("background-color", "#f5d34b");
+        $(".sidebar-nav > .sidebar-brand").css("background-color", "#f1c40f");
+        $(".badge-notify").css("color", "#a5a5a5");
+        $("#sidebar-wrapper").css("background-color", "#fff");
+        $(".sidebar-nav li a").css("border-bottom", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("border-right", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("color", "#34495E");
+        $(".nav-icon").css("color", "#C6C4DB")
+        $(".user-box p").css("color", "#fff");
+        $(".head-box i").css("color", "#fff");
+        $(".sidebar-nav > .sidebar-brand").css("color", "#fff");
+    });
+    $("#preview3").click(function () {
+        $("#head").css("background-color", "#62d995");
+        $("#searchTB ").css("background-color", "#62d995");
+        $(".sidebar-nav > .sidebar-brand").css("background-color", "#2ecc71");
+        $(".badge-notify").css("color", "#a5a5a5");
+        $("#sidebar-wrapper").css("background-color", "#fff");
+        $(".sidebar-nav li a").css("border-bottom", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("border-right", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("color", "#34495E");
+        $(".nav-icon").css("color", "#C6C4DB")
+        $(".user-box p").css("color", "#fff");
+        $(".head-box i").css("color", "#fff");
+        $(".sidebar-nav > .sidebar-brand").css("color", "#fff");
+    });
+    $("#preview4").click(function () {
+        $("#head").css("background-color", "#2b3643");
+        $("#searchTB ").css("background-color", "#2b3643");
+        $(".sidebar-nav > .sidebar-brand").css("background-color", "#303b49");
+        $(".badge-notify").css("color", "#a5a5a5");
+        $("#sidebar-wrapper").css("background-color", "#364150");
+        $(".sidebar-nav li a").css("border-bottom", "1px solid #3d4957");
+        $(".sidebar-nav li a").css("border-right", "1px solid #3d4957");
+        $(".sidebar-nav li a").css("color", "#b0b4c4");
+        $(".nav-icon").css("color", "#667287");
+        $(".user-box p").css("color", "#fff");
+        $(".head-box i").css("color", "#fff");
+        $(".sidebar-nav > .sidebar-brand").css("color", "#fff");
+    });
+    $("#preview5").click(function () {
+        $("#head").css("background-color", "#262930");
+        $("#searchTB ").css("background-color", "#262930");
+        $(".sidebar-nav > .sidebar-brand").css("background-color", "#282b32");
+        $(".badge-notify").css("color", "#a5a5a5");
+        $("#sidebar-wrapper").css("background-color", "#2a2e35");
+        $(".sidebar-nav li a").css("border-bottom", "1px solid #262930");
+        $(".sidebar-nav li a").css("border-right", "1px solid #262930");
+        $(".sidebar-nav li a").css("color", "#5b616d");
+        $(".nav-icon").css("color", "#767d8a");
+        $(".user-box p").css("color", "#fff");
+        $(".head-box i").css("color", "#fff");
+        $(".sidebar-nav > .sidebar-brand").css("color", "#fff");
+    });
+    $("#preview6").click(function () {
+        $("#head").css("background-color", "#fff");
+        $("#head").css("box-shadow", " 2px 2px 2px #ededed");
+        $("#searchTB ").css("background-color", "#828282");
+        $(".sidebar-nav > .sidebar-brand").css("background-color", "#ecf0f1");
+        $(".sidebar-nav > .sidebar-brand").css("color", "#686868");
+        $(".badge-notify").css("background-color", "#e0e0e0");
+        $(".badge-notify").css("color", "#a5a5a5");
+        $("#sidebar-wrapper").css("background-color", "#353535");
+        $(".sidebar-nav li a").css("border-bottom", "1px solid #fff");
+        $(".sidebar-nav li a").css("border-right", "1px solid #fff");
+        $(".sidebar-nav li a").css("color", "#6b6b6b");
+        $(".nav-icon").css("color", "#6b6b6b");
+        $(".user-box p").css("color", "#686868");
+        $(".head-box i").css("color", "#686868");
+    });
+});
