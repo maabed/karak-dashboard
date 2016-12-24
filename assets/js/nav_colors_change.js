a37a8a3543ba91b6aed1ba55c43a298a0ab53d41
Meshaal <mohammadz.meshaal@gmail.com>
2016-12-24 16:11:59 +0200

    right side swction

--- assets/js/nav_colors_change.js
+++ assets/js/nav_colors_change.js
@@ -2,7 +2,6 @@
         $("#head").css("background-color", "#3B97D2");
         $("#searchTB ").css("background-color", "#3B97D2");
         $("#company-name").css("background-color", "#2980B9");
-        $(".badge-notify").css("color", "#3B97D2");
         $("#sidebar-wrapper").css("background-color", "#fff");
         $(".sidebar-nav li a").css("border-bottom", "1px solid #E4E5E7");
         $(".sidebar-nav li a").css("border-right", "1px solid #E4E5E7");
@@ -17,7 +16,6 @@
         $("#head").css("background-color", "#f5d34b");
         $("#searchTB ").css("background-color", "#f5d34b");
         $("#company-name").css("background-color", "#f1c40f");
-        $(".badge-notify").css("color", "#a5a5a5");
         $("#sidebar-wrapper").css("background-color", "#fff");
         $(".sidebar-nav li a").css("border-bottom", "1px solid #E4E5E7");
         $(".sidebar-nav li a").css("border-right", "1px solid #E4E5E7");
@@ -32,7 +30,6 @@
         $("#head").css("background-color", "#62d995");
         $("#searchTB ").css("background-color", "#62d995");
         $("#company-name").css("background-color", "#2ecc71");
-        $(".badge-notify").css("color", "#a5a5a5");
         $("#sidebar-wrapper").css("background-color", "#fff");
         $(".sidebar-nav li a").css("border-bottom", "1px solid #E4E5E7");
         $(".sidebar-nav li a").css("border-right", "1px solid #E4E5E7");
@@ -47,7 +44,6 @@
         $("#head").css("background-color", "#2b3643");
         $("#searchTB ").css("background-color", "#2b3643");
         $("#company-name").css("background-color", "#303b49");
-        $(".badge-notify").css("color", "#a5a5a5");
         $("#sidebar-wrapper").css("background-color", "#364150");
         $(".sidebar-nav li a").css("border-bottom", "1px solid #3d4957");
         $(".sidebar-nav li a").css("border-right", "1px solid #3d4957");
@@ -62,7 +58,6 @@
         $("#head").css("background-color", "#262930");
         $("#searchTB ").css("background-color", "#262930");
         $("#company-name").css("background-color", "#282b32");
-        $(".badge-notify").css("color", "#a5a5a5");
         $("#sidebar-wrapper").css("background-color", "#2a2e35");
         $(".sidebar-nav li a").css("border-bottom", "1px solid #262930");
         $(".sidebar-nav li a").css("border-right", "1px solid #262930");
@@ -77,16 +72,15 @@
         $("#head").css("background-color", "#fff");
         $("#head").css("box-shadow", " 2px 2px 2px #ededed");
         $("#searchTB ").css("background-color", "#828282");
-        $("#company-name").css("background-color", "#ecf0f1");
-        $("#company-name").css("color", "#686868");
+        $("#company-name").css("background-color", "#828282");
+        $("#company-name").css("color", "#fff");
         $(".badge-notify").css("background-color", "#e0e0e0");
-        $(".badge-notify").css("color", "#a5a5a5");
-        $("#sidebar-wrapper").css("background-color", "#353535");
-        $(".sidebar-nav li a").css("border-bottom", "1px solid #fff");
-        $(".sidebar-nav li a").css("border-right", "1px solid #fff");
-        $(".sidebar-nav li a").css("color", "#6b6b6b");
-        $(".nav-icon").css("color", "#6b6b6b");
         $(".user-box p").css("color", "#686868");
         $(".head-box i").css("color", "#686868");
+         $("#sidebar-wrapper").css("background-color", "#fff");
+        $(".sidebar-nav li a").css("border-bottom", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("border-right", "1px solid #E4E5E7");
+        $(".sidebar-nav li a").css("color", "#34495E");
+        $(".nav-icon").css("color", "#C6C4DB")
         document.getElementById("company-name").style.opacity = "1";
     });
