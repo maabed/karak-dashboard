255fee49ac645cebbd84ffeecbf2ed207ea39bf7
RayanAmaratGT <ramarat@newrelease.io>
2017-01-03 09:21:33 +0200

    Fix review (#58)
    * index and forms changes
    
    * fast changes
    
    * fixed links and forms
    
    * merge remote
    
    * nav bar
    
    * small fixes
    
    * linked the search input to the search page
    
    * small changes
    
    * removed inline jquery in lists
    
    * review on search results page

--- assets/js/social.js
+++ assets/js/social.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
     $("#gif-start").click(function(){
         if(flag == 1){
             $("#gif-start").removeAttr("src");
-            $("#gif-start").attr("src","../assets/images/mobile-wireframe.gif");
+            $("#gif-start").attr("src","assets/images/mobile-wireframe.gif");
             flag = 0;
         }
         else{
             $("#gif-start").removeAttr("src");
-            $("#gif-start").attr("src","../assets/images/mobile-wireframe.png");
+            $("#gif-start").attr("src","assets/images/mobile-wireframe.png");
             flag = 1;  
         }
     });
