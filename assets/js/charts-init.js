a18d71cc1b73a6b6c5c004d94c10cb6668184e00
RayanGT <RayanGT>
2017-01-04 09:22:58 +0200

    fixes on the charts page

--- assets/js/charts-init.js
+++ assets/js/charts-init.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-    /*bar chart */
     
     
+    
+    /*Bar chart */
     var ctx = document.getElementById("bar");
     var bar = new Chart(ctx, {
         type: 'bar',
@@ -38,7 +39,10 @@ $(document).ready(function(){
                 }]
             }
         }
-    }); 
+    });
+    
+    
+    
     /* Pie charts */
     var ctx2 = document.getElementById("pie");
     var data = {
@@ -67,81 +71,40 @@ $(document).ready(function(){
     data: data,
     });
     
-        /* doughnut charts */
-
-});
-/* Line chart*/
-var ctx4 = document.getElementById("line");
-var myLineChart = new Chart(ctx4, {
-    type: 'line',
-    data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
-            spanGaps: false,
-        }
-    ]
-},
-            options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-
-});
-/*Radar chart*/
-var ctx5 = document.getElementById("radar");
-var myRadarChart = new Chart(ctx5, {
-    type: 'radar',
-    data: {
-    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-    datasets: [
-        {
-            label: "My First dataset",
-            backgroundColor: "rgba(179,181,198,0.2)",
-            borderColor: "rgba(179,181,198,1)",
-            pointBackgroundColor: "rgba(179,181,198,1)",
-            pointBorderColor: "#fff",
-            pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(179,181,198,1)",
-            data: [65, 59, 90, 81, 56, 55, 40]
+    
+    
+    /* Line chart*/
+    var ctx4 = document.getElementById("line");
+    var myLineChart = new Chart(ctx4, {
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fill: true,
+                    lineTension: 0.1,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    spanGaps: false,
+                }
+            ]
         },
-        {
-            label: "My Second dataset",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            pointBackgroundColor: "rgba(255,99,132,1)",
-            pointBorderColor: "#fff",
-            pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(255,99,132,1)",
-            data: [28, 48, 40, 19, 96, 27, 100]
-        }
-    ]
-},
-            options: {
+        options: {
             scales: {
                 yAxes: [{
                     ticks: {
@@ -150,122 +113,158 @@ var myRadarChart = new Chart(ctx5, {
                 }]
             }
         }
-});
-
-
-
+    });
+    
+    
+    /*Radar chart*/
+    var ctx5 = document.getElementById("radar");
+    var myRadarChart = new Chart(ctx5, {
+        type: 'radar',
+        data: {
+            labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    backgroundColor: "rgba(179,181,198,0.2)",
+                    borderColor: "rgba(179,181,198,1)",
+                    pointBackgroundColor: "rgba(179,181,198,1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(179,181,198,1)",
+                    data: [65, 59, 90, 81, 56, 55, 40]
+                },
+                {
+                    label: "My Second dataset",
+                    backgroundColor: "rgba(255,99,132,0.2)",
+                    borderColor: "rgba(255,99,132,1)",
+                    pointBackgroundColor: "rgba(255,99,132,1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(255,99,132,1)",
+                    data: [28, 48, 40, 19, 96, 27, 100]
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    
 /*google charts*/
-
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
+    
+    /*Stepped Area Chart*/
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Director (Year)',  'Rotten Tomatoes', 'IMDB'],
-          ['Alfred Hitchcock (1935)', 8.4,         7.9],
-          ['Ralph Thomas (1959)',     6.9,         6.5],
-          ['Don Sharp (1978)',        6.5,         6.4],
-          ['James Hawes (2008)',      4.4,         6.2]
+            ['Director (Year)',  'Rotten Tomatoes', 'IMDB'],
+            ['Alfred Hitchcock (1935)', 8.4,         7.9],
+            ['Ralph Thomas (1959)',     6.9,         6.5],
+            ['Don Sharp (1978)',        6.5,         6.4],
+            ['James Hawes (2008)',      4.4,         6.2]
         ]);
-
         var options = {
-          vAxis: {title: 'Accumulated Rating'},
-          isStacked: true
+            vAxis: {title: 'Accumulated Rating'},
+            isStacked: true
         };
-
         var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
-
         chart.draw(data, options);
-      }
-            
-            
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawpie);
-      function drawpie() {
-
+    }
+    
+    
+    /*Bubble chart*/
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawSeriesChart);
+    function drawSeriesChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+            ['ID', 'Life Expectancy', 'Fertility Rate', 'Population'],
+            ['DNK',    78.6,               1.84,     5523095],
+            ['EGY',    72.73,              2.78,     79716203],
+            ['IRN',    72.49,              1.7,      73137148],
+            ['IRQ',    68.09,              4.77,     31090763],
+            ['JOR',    81.55,              2.96,     7485600],
+            ['RUS',    68.6,               1.54,     141850000],
+            ['USA',    78.09,              2.05,     307007000]
         ]);
-
         var options = {
+            title: 'Correlation between life expectancy, fertility rate ' +
+            'and population of some world countries (2010)',
+            hAxis: {title: 'Life Expectancy'},
+            vAxis: {title: 'Fertility Rate'},
+            bubble: {textStyle: {fontSize: 11}}
         };
-
-        var chart = new google.visualization.PieChart(document.getElementById('google_pie'));
-
+        var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
         chart.draw(data, options);
-      }
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawcandle);
-  function drawcandle() {
-    var data = google.visualization.arrayToDataTable([
-      ['Mon', 20, 28, 38, 45],
-      ['Tue', 31, 38, 55, 66],
-      ['Wed', 50, 55, 77, 80],
-      ['Thu', 77, 77, 66, 50],
-      ['Fri', 68, 66, 22, 15]
-      // Treat first row as data as well.
-    ], true);
-
-    var options = {
-      legend:'none'
-    };
-
-    var chart = new google.visualization.CandlestickChart(document.getElementById('candlestick'));
-
-    chart.draw(data, options);
-  }
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawLine);
-
-      function drawLine() {
+    }
+    
+    
+    /*Candlestick chart*/
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawcandle);
+    function drawcandle() {
         var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
-
+            ['Mon', 20, 28, 38, 45],
+            ['Tue', 31, 38, 55, 66],
+            ['Wed', 50, 55, 77, 80],
+            ['Thu', 77, 77, 66, 50],
+            ['Fri', 68, 66, 22, 15]
+            // Treat first row as data as well.
+        ], true);
         var options = {
-          title: 'Company Performance',
-          curveType: 'function',
-          legend: { position: 'bottom' }
+            legend:'none'
+        }; 
+        var chart = new google.visualization.CandlestickChart(document.getElementById('candlestick'));
+        chart.draw(data, options);
+    }
+    
+    
+    /*Line chart*/
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawLine);
+    function drawLine() {
+        var data = google.visualization.arrayToDataTable([
+            ['Year', 'Sales', 'Expenses'],
+            ['2004',  1000,      400],
+            ['2005',  1170,      460],
+            ['2006',  660,       1120],
+            ['2007',  1030,      540]
+        ]);
+        var options = {
+            title: 'Company Performance',
+            curveType: 'function',
+            legend: { position: 'bottom' }
         };
-
         var chart = new google.visualization.LineChart(document.getElementById('google_line'));
-
         chart.draw(data, options);
       }
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(scatter);
-      function scatter() {
+    
+    
+    /*Scatter chart*/
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(scatter);
+    function scatter() {
         var data = google.visualization.arrayToDataTable([
-          ['Age', 'Weight'],
-          [ 8,      12],
-          [ 4,      5.5],
-          [ 11,     14],
-          [ 4,      5],
-          [ 3,      3.5],
-          [ 6.5,    7]
+            ['Age', 'Weight'],
+            [ 8,      12],
+            [ 4,      5.5],
+            [ 11,     14],
+            [ 4,      5],
+            [ 3,      3.5],
+            [ 6.5,    7]
         ]);
-
         var options = {
-          title: 'Age vs. Weight comparison',
-          hAxis: {title: 'Age', minValue: 0, maxValue: 15},
-          vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
-          legend: 'none'
+            title: 'Age vs. Weight comparison',
+            hAxis: {title: 'Age', minValue: 0, maxValue: 15},
+            vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
+            legend: 'none'
         };
-
         var chart = new google.visualization.ScatterChart(document.getElementById('scatter'));
-
         chart.draw(data, options);
       }
-
-
-
-
-
+});
