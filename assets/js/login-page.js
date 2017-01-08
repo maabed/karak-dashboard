6c680bc122bb5999838b1ec149c485126ecbd914
mohammadmeshaal <mohammadz.meshaal@gmail.com>
2017-01-08 19:20:30 +0200

    login and registration page rebuild

---
+++ assets/js/login-page.js
@@ -0,0 +1,7 @@
+﻿$('.toggle').on('click', function () {
+    $('.container').stop().addClass('active');
+});
+
+$('.close').on('click', function () {
+    $('.container').stop().removeClass('active');
+});
