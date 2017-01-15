84394a082dda6936f064597bc16bc459d1c1dde2
RayanGT <RayanGT>
2017-01-15 14:46:57 +0200

    overall changes to the page

--- assets/js/login-page.js
+++ assets/js/login-page.js
@@ -1,7 +1,17 @@
-﻿$('.toggle').on('click', function () {
+$('.toggle').on('click', function () {
     $('.container').stop().addClass('active');
 });
 
 $('.close').on('click', function () {
     $('.container').stop().removeClass('active');
+});
+$("#forgot").click(function(){
+    $('.toggle').css("display","none");
+    $('.login').css("display","none");
+    $('.forgot').fadeIn("slow");
+});
+$("#login").click(function(){
+    $('.forgot').css("display","none");
+    $('.toggle').fadeIn("slow");
+    $('.login').fadeIn("slow");
 });
