0cf53f1427d95f040b2b7a43bab589e5608ab3b5
mohammadmeshaal <mmeshaal@newrelease.io>
2017-01-17 12:11:31 +0200

    (R2/login) (#65)
    * login and registration page rebuild
    
    * login and registration page rebuild
    
    * overall changes to the page

---
+++ assets/js/login-page.js
@@ -0,0 +1,17 @@
+$('.toggle').on('click', function () {
+    $('.container').stop().addClass('active');
+});
+
+$('.close').on('click', function () {
+    $('.container').stop().removeClass('active');
+});
+$("#forgot").click(function(){
+    $('.toggle').css("display","none");
+    $('.login').css("display","none");
+    $('.forgot').fadeIn("slow");
+});
+$("#login").click(function(){
+    $('.forgot').css("display","none");
+    $('.toggle').fadeIn("slow");
+    $('.login').fadeIn("slow");
+});
