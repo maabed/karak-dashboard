d806eb06209d61e4bff6e33c29a7125f41c0d35d
RayanAmaratGT <ramarat@newrelease.io>
2017-01-26 11:46:48 +0200

    Modals : overall changes to the modals page (R2/modals) (#64)
    * overall changes to the modals page
    
    * changed some of the forms properties
    
    * changes on images and descriptions
    
    * Bug fixes
    
    * added a footer
    
    * added content to small modal and small changes to right side modal
    
    * changed the right side buttons position and width
    
    * fixed the spaces between the buttons

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,74 +1,9 @@
-$(document).ready(function(){
-    $('#standard-parent').mouseenter(function(){
-        $('#view-standard').fadeIn(50);
-        $('#standard').css('opacity','0.5');
-    });
-    $('#standard-parent').mouseleave(function(){
-        $('#view-standard').fadeOut(50);
-        $('#standard').css('opacity','1');
-    });
-    
-    $('#view-standard').click(function(){
-        $('#view-standard').removeClass('view-modal');
-    });
-    
-    
-    $('#flat-parent').mouseenter(function(){
-        $('#view-flat').fadeIn(50);
-        $('#flat').css('opacity','0.5');
-    });
-    $('#flat-parent').mouseleave(function(){
-        $('#view-flat').fadeOut(50);
-            $('#flat').css('opacity','1');
-    });
-    $('#view-flat').click(function(){
-        $('#view-flat').removeClass('view-modal');
-        $('#flat').css('opacity','1');
-    });
-    
-    
-    
-    $('#small-parent').mouseenter(function(){
-        $('#view-small').fadeIn(50);
-        $('#small').css('opacity','0.5');
-    });
-    $('#small-parent').mouseleave(function(){
-        $('#view-small').fadeOut(50);
-        $('#small').css('opacity','1');
-    });
-    $('#view-small').click(function(){
-        $('#view-small').removeClass('view-modal');
-        $('#small').css('opacity','1');
-    });
-    
-    
-    $('#data-parent').mouseenter(function(){
-        $('#view-data').fadeIn(50);
-        $('#data').css('opacity','0.5');
-    });
-    $('#data-parent').mouseleave(function(){
-        $('#view-data').fadeOut(50);
-        $('#data').css('opacity','1');
-    });
-    $('#view-data').click(function(){
-        $('#view-data').removeClass('view-modal');
-        $('#data').css('opacity','1');
-    });
-    
-    
-    
-    $('#right-parent').mouseenter(function(){
-        $('#view-right').fadeIn(50);
-        $('#right').css('opacity','0.5');
-    });
-    $('#right-parent').mouseleave(function(){
-        $('#view-right').fadeOut(50);
-        $('#right').css('opacity','1');
-    });
-    $('#view-right').click(function(){
-        $('#view-right').removeClass('view-modal');
-        $('#right').css('opacity','1');
-
-    });
-    
-});
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+})
