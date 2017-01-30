d7c787256f2bdbaf37a46e901ec6ca993bab8f96
Meshaal <mohammadz.meshaal@gmail.com>
2017-01-30 15:09:14 +0200

    landing page finishing

--- assets/js/landing_page.js
+++ assets/js/landing_page.js
@@ -60,4 +60,30 @@ function scaleBannerVideoSize(element){
         $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
 
     });
+
+
+    $('.custom1').owlCarousel({
+        items: 1,
+        margin: 30,
+        stagePadding: 30,
+        smartSpeed: 450,
+        nav: true,
+        loop:true
+    });
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1500);
+                    return false;
+                }
+            }
+        });
+    });
+
 }
+
