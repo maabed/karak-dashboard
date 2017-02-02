99dc861218f2de8844c38776f4b84cea7fcfba66
Meshaal <mohammadz.meshaal@gmail.com>
2017-02-02 19:40:20 +0200

    chat view

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
-$(".submenu-li a i").css("font-size", "0.8em");
-$(".submenu-li a i").css("margin-top", "1.3em");
+$(document).ready(function() {
+    $(".submenu-li a i").css("font-size", "0.8em");
+    $(".submenu-li a i").css("margin-top", "1.3em");
+});
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$('.SubMenue').hide();
+
+$('.MainMenue').click(function(e) {
+    e.preventDefault();
+    var $this = $(this).parent().find('.Submenue');
+
+    // here is what I want to do
+    $this.toggle(300);
+});
+$("#left-Side-Section-Togle").click(function() {
+    $(".InboxLeftSide").toggleClass("show-left");
 });
