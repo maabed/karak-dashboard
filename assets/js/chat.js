113179cd71e36c24933a6b40719cbefc0f406a76
Meshaal <mohammadz.meshaal@gmail.com>
2017-03-07 17:26:43 +0200

    chat page full review

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -6,35 +6,37 @@ http://newrelease.io
 Copyright 2017 New Release
 */
 $(function($) {
-  'use strict';
-  $(document).ready(function() {
-    $('.submenu-li a i').css({
-      'font-size': '0.8em',
-      'margin-top': '1.3em'
+    'use strict';
+    $(document).ready(function() {
+        $('.submenu-li a i').css({
+            'font-size': '0.8em',
+            'margin-top': '1.3em'
+        });
     });
-  });
-  $('#menu-toggle').click(function(e) {
-    e.preventDefault();
-    $('#wrapper').toggleClass('toggled');
-  });
-  $('.SubMenue').hide();
-  $('.open-sub').click(function(e) {
-    e.preventDefault();
-    var $this = $(this).parent().parent().find('.Submenue');
-    $this.toggle(300);
-  });
-  $('#left-Side-Section-Togle').click(function() {
-    $('.InboxLeftSide').toggleClass('show-left');
-  });
+    $('#menu-toggle').click(function(e) {
+        e.preventDefault();
+        $('#wrapper').toggleClass('toggled');
+    });
+    $('.SubMenue').hide();
+    $('.open-sub').click(function(e) {
+        e.preventDefault();
+        var $this = $(this).parent().parent().find('.Submenue');
+        $this.toggle(300);
+    });
 
-  $('input:checkbox').on('click', function() {
-    var $box = $(this);
-    if ($box.is(':checked')) {
-      var group = "input:checkbox[name='" + $box.attr("name") + "']";
-      $(group).prop('checked', false);
-      $box.prop('checked', true);
-    } else {
-      $box.prop('checked', false);
-    }
-  });
+    $('input:checkbox').on('click', function() {
+        var $box = $(this);
+        if ($box.is(':checked')) {
+            var group = "input:checkbox[name='" + $box.attr("name") + "']";
+            $(group).prop('checked', false);
+            $box.prop('checked', true);
+        } else {
+            $box.prop('checked', false);
+        }
+    });
 });
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+}
