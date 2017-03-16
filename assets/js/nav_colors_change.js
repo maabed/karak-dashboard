b6885b0b272227ce79a7e6103e589eea5f40bd8b
Meshaal <mohammadz.meshaal@gmail.com>
2017-03-16 16:28:41 +0200

    fix issues

--- assets/js/nav_colors_change.js
+++ assets/js/nav_colors_change.js
@@ -15,16 +15,16 @@ $(function($) {
       $('.search-text-box ').css('background-color', '#3B97D2');
       $('#company-name').css('background-color', '#2980B9');
       $('#sidebar-wrapper').css('background-color', '#fff');
-      $('.sidebar-nav li a').css('border-bottom', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('border-right', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('color', '#34495E');
-      $('.nav-icon').css('color', '#C6C4DB')
+      $('.sidebar li a').css('border-bottom', '1px solid #E4E5E7');
+      $('.sidebar li a').css('border-right', '1px solid #E4E5E7');
+      $('.sidebar li a').css('color', '#34495E');
+      $('.nav-icon').css('color', '#C6C4DB');
       $('.user-box p').css('color', '#fff');
       $('.head-box i').css('color', '#fff');
       $('#company-name').css('color', '#fff');
-      $('.sidebar-nav .selected a').css('border-left', '4px solid #3B97D2');
+      $('.sidebar .selected a').css('border-left', '4px solid #3B97D2');
       $('.body-header').css('border-left', '5px solid #3B97D2');
-      $('.sidebar-nav .selected a .nav-icon ').css('color', '#3B97D2');
+      $('.sidebar .selected a .nav-icon ').css('color', '#3B97D2');
     });
 
     $('#preview2').click(function() {
@@ -32,16 +32,16 @@ $(function($) {
       $('.search-text-box ').css('background-color', '#f5d34b');
       $('#company-name').css('background-color', '#f1c40f');
       $('#sidebar-wrapper').css('background-color', '#fff');
-      $('.sidebar-nav li a').css('border-bottom', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('border-right', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('color', '#34495E');
-      $('.nav-icon').css('color', '#C6C4DB')
+      $('.sidebar li a').css('border-bottom', '1px solid #E4E5E7');
+      $('.sidebar li a').css('border-right', '1px solid #E4E5E7');
+      $('.sidebar li a').css('color', '#34495E');
+      $('.nav-ic[on').css('color', '#C6C4DB');
       $('.user-box p').css('color', '#fff');
       $('.head-box i').css('color', '#fff');
       $('#company-name').css('color', '#fff');
-      $('.sidebar-nav .selected a').css('border-left', '4px solid #f5d34b');
+      $('.sidebar .selected a').css('border-left', '4px solid #f5d34b');
       $('.body-header').css('border-left', '5px solid #f5d34b');
-      $('.sidebar-nav .selected a .nav-icon ').css('color', '#f5d34b');
+      $('.sidebar .selected a .nav-icon ').css('color', '#f5d34b');
     });
 
 
@@ -50,16 +50,16 @@ $(function($) {
       $('.search-text-box ').css('background-color', '#62d995');
       $('#company-name').css('background-color', '#2ecc71');
       $('#sidebar-wrapper').css('background-color', '#fff');
-      $('.sidebar-nav li a').css('border-bottom', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('border-right', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('color', '#34495E');
-      $('.nav-icon').css('color', '#C6C4DB')
+      $('.sidebar li a').css('border-bottom', '1px solid #E4E5E7');
+      $('.sidebar li a').css('border-right', '1px solid #E4E5E7');
+      $('.sidebar li a').css('color', '#34495E');
+      $('.nav-icon').css('color', '#C6C4DB');
       $('.user-box p').css('color', '#fff');
       $('.head-box i').css('color', '#fff');
       $('#company-name').css('color', '#fff');
-      $('.sidebar-nav .selected a').css('border-left', '4px solid #62d995');
+      $('.sidebar .selected a').css('border-left', '4px solid #62d995');
       $('.body-header').css('border-left', '5px solid #62d995');
-      $('.sidebar-nav .selected a .nav-icon ').css('color', '#62d995');
+      $('.sidebar .selected a .nav-icon ').css('color', '#62d995');
     });
 
     $('#preview4').click(function() {
@@ -67,16 +67,16 @@ $(function($) {
       $('.search-text-box ').css('background-color', '#2b3643');
       $('#company-name').css('background-color', '#303b49');
       $('#sidebar-wrapper').css('background-color', '#364150');
-      $('.sidebar-nav li a').css('border-bottom', '1px solid #3d4957');
-      $('.sidebar-nav li a').css('border-right', '1px solid #3d4957');
-      $('.sidebar-nav li a').css('color', '#b0b4c4');
+      $('.sidebar li a').css('border-bottom', '1px solid #3d4957');
+      $('.sidebar li a').css('border-right', '1px solid #3d4957');
+      $('.sidebar li a').css('color', '#b0b4c4');
       $('.nav-icon').css('color', '#667287');
       $('.user-box p').css('color', '#fff');
       $('.head-box i').css('color', '#fff');
       $('#company-name').css('color', '#fff');
-      $('.sidebar-nav .selected a').css('border-left', '4px solid #2b3643');
+      $('.sidebar .selected a').css('border-left', '4px solid #2b3643');
       $('.body-header').css('border-left', '5px solid #2b3643');
-      $('.sidebar-nav .selected a .nav-icon ').css('color', '#2b3643');
+      $('.sidebar .selected a .nav-icon ').css('color', '#2b3643');
     });
 
     $('#preview5').click(function() {
@@ -84,16 +84,16 @@ $(function($) {
       $('.search-text-box ').css('background-color', '#262930');
       $('#company-name').css('background-color', '#282b32');
       $('#sidebar-wrapper').css('background-color', '#2a2e35');
-      $('.sidebar-nav li a').css('border-bottom', '1px solid #262930');
-      $('.sidebar-nav li a').css('border-right', '1px solid #262930');
-      $('.sidebar-nav li a').css('color', '#5b616d');
+      $('.sidebar li a').css('border-bottom', '1px solid #262930');
+      $('.sidebar li a').css('border-right', '1px solid #262930');
+      $('.sidebar li a').css('color', '#5b616d');
       $('.nav-icon').css('color', '#767d8a');
       $('.user-box p').css('color', '#fff');
       $('.head-box i').css('color', '#fff');
       $('#company-name').css('color', '#fff');
-      $('.sidebar-nav .selected a').css('border-left', '4px solid #262930');
+      $('.sidebar .selected a').css('border-left', '4px solid #262930');
       $('.body-header').css('border-left', '5px solid #262930');
-      $('.sidebar-nav .selected a .nav-icon ').css('color', '#262930');
+      $('.sidebar .selected a .nav-icon ').css('color', '#262930');
     });
 
     $('#preview6').click(function() {
@@ -106,13 +106,13 @@ $(function($) {
       $('.user-box p').css('color', '#686868');
       $('.head-box i').css('color', '#686868');
       $('#sidebar-wrapper').css('background-color', '#fff');
-      $('.sidebar-nav li a').css('border-bottom', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('border-right', '1px solid #E4E5E7');
-      $('.sidebar-nav li a').css('color', '#34495E');
+      $('.sidebar li a').css('border-bottom', '1px solid #E4E5E7');
+      $('.sidebar li a').css('border-right', '1px solid #E4E5E7');
+      $('.sidebar li a').css('color', '#34495E');
       $('.nav-icon').css('color', '#C6C4DB')
-      $('.sidebar-nav .selected a').css('border-left', '4px solid #828282');
+      $('.sidebar .selected a').css('border-left', '4px solid #828282');
       $('.body-header').css('border-left', '5px solid #828282');
-      $('.sidebar-nav .selected a .nav-icon ').css('color', '#828282');
+      $('.sidebar .selected a .nav-icon ').css('color', '#828282');
     });
   });
 });
