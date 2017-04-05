38594a65420ef35d90b47e8b3fff9d94a74beab0
Rayan <ramarat@newrelease.io>
2017-04-05 14:16:47 +0300

    side bar data are now rendered from array of object and finished the header

--- demo/meteor/client/components/side-bar/side-bar.js
+++ demo/meteor/client/components/side-bar/side-bar.js
@@ -1,3 +1,199 @@
+Template.sidebar.helpers( {
+   nav : [
+    {
+    link : "index.html",
+    icon : "fa-tachometer",
+    text : "Dashboard",
+    hassub: 1
+    },
+    {
+      link : "#",
+      icon: "fa-file-text",
+      text : "Forms",
+      hassub: 0,
+
+      submenu : [
+      {
+        link : "forms.html",
+        text : "Form Elements"
+      },
+      {
+        link : "form_wizard.html",
+        text : "Form Wizard"
+      },
+      ],
+    },
+    {
+      link : "#",
+      icon: "fa-pie-chart",
+      text : "Charts",
+      hassub: 1
+    },
+    {
+      link: "inbox.html",
+      icon : "fa-envelope",
+      text : "Email",
+      hassub: 1
+    },
+    {
+      link: "#",
+      icon : "fa-eye ",
+      text : "UI Elements",
+      hassub: 0,
+
+      submenu : [
+      {
+        link : "icons.html",
+        text : "Icons"
+      },
+      {
+        link : "colors.html",
+        text : "Colors"
+      },
+      {
+        link : "buttons.html",
+        text : "Buttons"
+      },
+      {
+        link : "Notifications.html",
+        text : "Notifications"
+      },
+            {
+        link : "modals.html",
+        text : "Modals"
+      },
+      {
+        link : "tabs.html",
+        text : "Tabs"
+      },
+      {
+        link : "cards.html",
+        text : "Cards"
+      },
+            {
+        link : "loading.html",
+        text : "Loading"
+      },
+      {
+        link : "panels.html",
+        text : "Panels"
+      },
+      {
+        link : "lists.html",
+        text : "Lists"
+      },
+      {
+        link : "menus.html",
+        text : "Menus"
+      },
+      {
+        link : "hover.html",
+        text : "Hover"
+      },
+      ],
+
+    },
+    {
+      link: "calendar.html",
+      icon : "fa-calendar-check-o",
+      text : "Calendar",
+      hassub: 1
+    },
+    {
+      link: "#",
+      icon : "fa-star",
+      text : "App Views",
+      hassub: 0,
+
+      submenu : [
+      {
+        link : "Budget.html",
+        text : "Budget"
+      },
+      {
+        link : "chat.html",
+        text : "Chat"
+      },
+      {
+        link : "social.html",
+        text : "Social"
+      },
+      {
+        link : "landing_page.html",
+        text : "Landing Page"
+      },
+      ],
+    },
+    {
+      link: "#",
+      icon : "fa-table",
+      text : "Tables",
+      hassub: 0,
+
+      submenu : [
+      {
+        link : "tables.html",
+        text : "Basic Table"
+      },
+      {
+        link : "data_tables.html",
+        text : "Data Table"
+      },
+      ]
+    },
+    {
+      link: "#",
+      icon : "fa-map",
+      text : "Maps",
+      hassub: 0,
+
+      submenu : [
+      {
+        link : "map.html",
+        text : "Vector Maps"
+      },
+      {
+        link : "gogole_map.html",
+        text : "Google Map"
+      },
+      ]
+    },
+    {
+      link: "#",
+      icon : "fa-gift",
+      text : "Extra",
+      hassub: 0,
+
+      submenu : [
+      {
+        link : "invoice.html",
+        text : "Invoice"
+      },
+      {
+        link : "error400.html",
+        text : "400 Page"
+      },
+      {
+        link : "error500.html",
+        text : "500 Page"
+      },
+      {
+        link : "login.html",
+        text : "Login & Registration"
+      },
+      {
+        link : "lock_screen.html",
+        text : "Lock Screen"
+      },
+      {
+        link : "tour.html",
+        text : "Tour"
+      },
+      ]
+    }
+
+  ],
+});
 
 $(document).ready(function() {
  $('#menu-toggle').click(function(e) {
