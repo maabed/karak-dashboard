6dfa5a1a7ff181717d58815b7149efd5820f0a5b
Rayan <ramarat@newrelease.io>
2017-04-08 19:28:09 +0300

    commit by mahmous

--- demo/meteor/client/components/header/header.js
+++ demo/meteor/client/components/header/header.js
@@ -1,61 +1,52 @@
-Template.header.helpers( {
-
-    notifications : [
-    {
-        image : "/images/user_01_54.jpg",
-        name : "Mohammad Meshaal",
-        notification : "wants your confirmation on his Leave Request on 12 Mar 2017"
-    },
-    {
-        image : "/images/user_04_54.jpg",
-        name : "Nasir ahmad",
-        notification : "sent a payment on 11 Mar 2017"
-    },
-    ],
-    messages : [
-    {
-        image : "/images/user_02_54.jpg",
-        name : "Layla smith",
-        message : "Job offer"
-    },
-    {
-        image : "/images/user_01_54.jpg",
-        name : "Mohammad Meshaal",
-        message : "Please send me the work templates"
-    },
-    {
-        image : "/images/user_03_54.jpg",
-        name : "Dana khalid",
-        message : "Do you wanna hang out today?"
-    },
-    {
-        image : "/images/user_04_54.jpg",
-        name : "Nasir ahmad",
-        message : "Are we going to the match today?"
-    },
-    ],
+Template.header.helpers({
+    notifications: [{
+        image: '/images/user_01_54.jpg',
+        name: 'Mohammad Meshaal',
+        notification: 'wants your confirmation on his Leave Request on 12 Mar 2017',
+    }, {
+        image: '/images/user_04_54.jpg',
+        name: 'Nasir ahmad',
+        notification: 'sent a payment on 11 Mar 2017',
+    }, ],
+    messages: [{
+        image: '/images/user_02_54.jpg',
+        name: 'Layla smith',
+        message: 'Job offer',
+    }, {
+        image: '/images/user_01_54.jpg',
+        name: 'Mohammad Meshaal',
+        message: 'Please send me the work templates',
+    }, {
+        image: '/images/user_03_54.jpg',
+        name: 'Dana khalid',
+        message: 'Do you wanna hang out today?',
+    }, {
+        image: '/images/user_04_54.jpg',
+        name: 'Nasir ahmad',
+        message: 'Are we going to the match today?',
+    }, ],
 });
 
 $(document).ready(function() {
-    $("#notifications").click(function() {
-        $(".notifecations-box").fadeToggle();
-        $(".messages-box").fadeOut();
-        $("#not-badg").fadeOut();
-        $(".user-info-box").fadeOut();
+    $('#notifications').click(function() {
+        $('.notifecations-box').fadeToggle();
+        $('.messages-box').fadeOut();
+        $('#not-badg').fadeOut();
+        $('.user-info-box').fadeOut();
     });
-    $("#messages").click(function() {
-        $(".messages-box").fadeToggle();
-        $(".notifecations-box").fadeOut();
-        $("#message-badg").fadeOut();
-        $(".user-info-box").fadeOut();
+    $('#messages').click(function() {
+        $('.messages-box').fadeToggle();
+        $('.notifecations-box').fadeOut();
+        $('#message-badg').fadeOut();
+        $('.user-info-box').fadeOut();
     });
-    $("#user-box").click(function() {
-        $(".user-info-box").fadeToggle();
-        $(".notifecations-box").fadeOut();
-        $(".messages-box").fadeOut();
+    $('#user-box').click(function() {
+        $('.user-info-box').fadeToggle();
+        $('.notifecations-box').fadeOut();
+        $('.messages-box').fadeOut();
     });
     $(document).mouseup(function(e) {
-        var container = $(".notifecations-box, .messages-box, .user-info-box");
+        var container = $('.notifecations-box, .messages-box, .user-info-box');
         if (!container.is(e.target) && container.has(e.target).length === 0) {
             container.hide();
         }
--- demo/meteor/client/components/side-bar/side-bar.js
+++ demo/meteor/client/components/side-bar/side-bar.js
@@ -1,198 +1,193 @@
-Template.sidebar.helpers( {
-   nav : [
-    {
-    link : "index.html",
-    icon : "fa-tachometer",
-    text : "Dashboard",
-    hassub: 1
-    },
-    {
-      link : "#",
-      icon: "fa-file-text",
-      text : "Forms",
-      hassub: 0,
+Template.sidebar.helpers({
+  menuItem() {
+    const menuItem = [
+      {
+        link: 'index.html',
+        icon: 'fa-tachometer',
+        text: 'Dashboard',
+        hassub: 1,
+      },
+      {
+        link: '#',
+        icon: 'fa-file-text',
+        text: 'Forms',
+        hassub: 0,
 
-      submenu : [
-      {
-        link : "forms.html",
-        text : "Form Elements"
+        submenu: [{
+          link: 'forms.html',
+          text: 'Form Elements',
+        },
+        {
+          link: 'form_wizard.html',
+          text: 'Form Wizard',
+        },
+        ],
       },
       {
-        link : "form_wizard.html",
-        text : "Form Wizard"
+        link: '#',
+        icon: 'fa-pie-chart',
+        text: 'Charts',
+        hassub: 1,
       },
-      ],
-    },
-    {
-      link : "#",
-      icon: "fa-pie-chart",
-      text : "Charts",
-      hassub: 1
-    },
-    {
-      link: "inbox.html",
-      icon : "fa-envelope",
-      text : "Email",
-      hassub: 1
-    },
-    {
-      link: "#",
-      icon : "fa-eye ",
-      text : "UI Elements",
-      hassub: 0,
+      {
+        link: 'inbox.html',
+        icon: 'fa-envelope',
+        text: 'Email',
+        hassub: 1,
+      },
+      {
+        link: '#',
+        icon: 'fa-eye ',
+        text: 'UI Elements',
+        hassub: 0,
 
-      submenu : [
-      {
-        link : "icons.html",
-        text : "Icons"
-      },
-      {
-        link : "colors.html",
-        text : "Colors"
-      },
-      {
-        link : "buttons.html",
-        text : "Buttons"
-      },
-      {
-        link : "Notifications.html",
-        text : "Notifications"
-      },
-            {
-        link : "modals.html",
-        text : "Modals"
-      },
-      {
-        link : "tabs.html",
-        text : "Tabs"
-      },
-      {
-        link : "cards.html",
-        text : "Cards"
-      },
-            {
-        link : "loading.html",
-        text : "Loading"
-      },
-      {
-        link : "panels.html",
-        text : "Panels"
-      },
-      {
-        link : "lists.html",
-        text : "Lists"
-      },
-      {
-        link : "menus.html",
-        text : "Menus"
-      },
-      {
-        link : "hover.html",
-        text : "Hover"
-      },
-      ],
+        submenu: [{
+          link: 'icons.html',
+          text: 'Icons',
+        },
+        {
+          link: 'colors.html',
+          text: 'Colors',
+        },
+        {
+          link: 'buttons.html',
+          text: 'Buttons',
+        },
+        {
+          link: 'Notifications.html',
+          text: 'Notifications',
+        },
+        {
+          link: 'modals.html',
+          text: 'Modals',
+        },
+        {
+          link: 'tabs.html',
+          text: 'Tabs',
+        },
+        {
+          link: 'cards.html',
+          text: 'Cards',
+        },
+        {
+          link: 'loading.html',
+          text: 'Loading',
+        },
+        {
+          link: 'panels.html',
+          text: 'Panels',
+        },
+        {
+          link: 'lists.html',
+          text: 'Lists',
+        },
+        {
+          link: 'menus.html',
+          text: 'Menus',
+        },
+        {
+          link: 'hover.html',
+          text: 'Hover',
+        },
+        ],
 
-    },
-    {
-      link: "calendar.html",
-      icon : "fa-calendar-check-o",
-      text : "Calendar",
-      hassub: 1
-    },
-    {
-      link: "#",
-      icon : "fa-star",
-      text : "App Views",
-      hassub: 0,
+      },
+      {
+        link: 'calendar.html',
+        icon: 'fa-calendar-check-o',
+        text: 'Calendar',
+        hassub: 1,
+      },
+      {
+        link: '#',
+        icon: 'fa-star',
+        text: 'App Views',
+        hassub: 0,
 
-      submenu : [
-      {
-        link : "Budget.html",
-        text : "Budget"
+        submenu: [{
+          link: 'Budget.html',
+          text: 'Budget',
+        },
+        {
+          link: 'chat.html',
+          text: 'Chat',
+        },
+        {
+          link: 'social.html',
+          text: 'Social',
+        },
+        {
+          link: 'landing_page.html',
+          text: 'Landing Page',
+        },
+        ],
       },
       {
-        link : "chat.html",
-        text : "Chat"
-      },
-      {
-        link : "social.html",
-        text : "Social"
-      },
-      {
-        link : "landing_page.html",
-        text : "Landing Page"
-      },
-      ],
-    },
-    {
-      link: "#",
-      icon : "fa-table",
-      text : "Tables",
-      hassub: 0,
+        link: '#',
+        icon: 'fa-table',
+        text: 'Tables',
+        hassub: 0,
 
-      submenu : [
-      {
-        link : "tables.html",
-        text : "Basic Table"
+        submenu: [{
+          link: 'tables.html',
+          text: 'Basic Table',
+        },
+        {
+          link: 'data_tables.html',
+          text: 'Data Table',
+        },
+        ],
       },
       {
-        link : "data_tables.html",
-        text : "Data Table"
-      },
-      ]
-    },
-    {
-      link: "#",
-      icon : "fa-map",
-      text : "Maps",
-      hassub: 0,
+        link: '#',
+        icon: 'fa-map',
+        text: 'Maps',
+        hassub: 0,
 
-      submenu : [
-      {
-        link : "map.html",
-        text : "Vector Maps"
+        submenu: [{
+          link: 'map.html',
+          text: 'Vector Maps',
+        },
+        {
+          link: 'gogole_map.html',
+          text: 'Google Map',
+        },
+        ],
       },
       {
-        link : "gogole_map.html",
-        text : "Google Map"
-      },
-      ]
-    },
-    {
-      link: "#",
-      icon : "fa-gift",
-      text : "Extra",
-      hassub: 0,
+        link: '#',
+        icon: 'fa-gift',
+        text: 'Extra',
+        hassub: 0,
 
-      submenu : [
-      {
-        link : "invoice.html",
-        text : "Invoice"
-      },
-      {
-        link : "error400.html",
-        text : "400 Page"
-      },
-      {
-        link : "error500.html",
-        text : "500 Page"
-      },
-      {
-        link : "login.html",
-        text : "Login & Registration"
-      },
-      {
-        link : "lock_screen.html",
-        text : "Lock Screen"
-      },
-      {
-        link : "tour.html",
-        text : "Tour"
-      },
-      ]
-    }
-
-  ],
+        submenu: [{
+          link: 'invoice.html',
+          text: 'Invoice',
+        },
+        {
+          link: 'error400.html',
+          text: '400 Page',
+        },
+        {
+          link: 'error500.html',
+          text: '500 Page',
+        },
+        {
+          link: 'login.html',
+          text: 'Login & Registration',
+        },
+        {
+          link: 'lock_screen.html',
+          text: 'Lock Screen',
+        },
+        {
+          link: 'tour.html',
+          text: 'Tour',
+        },
+        ],
+      }];
+    return menuItem;
+  },
 });
 
 $(document).ready(function() {
@@ -221,21 +216,21 @@ $(document).ready(function() {
  $('.sidebar').find('li.selected').find('.sidebar-submenue').css('display', 'block');
 
 
- $('.sidebar-submenue > li > a').not( ".chat-view-navbar .sidebar-submenue > li > a").css('margin-left', '40px');
+ $('.sidebar-submenue > li > a').not( '.chat-view-navbar .sidebar-submenue > li > a').css('margin-left', '40px');
 
  $('.has-submenu > a').click(function(){
   var current = $(this);
   var parent = $(this).parent().parent();
   var li = $(this).parent();
-  if(li.hasClass("open active")){
-    current.children('.left-arrow').removeClass("open");
-    li.removeClass("open active");  
+  if(li.hasClass('open active')){
+    current.children('.left-arrow').removeClass('open');
+    li.removeClass('open active');  
   }
   else{
     parent.children('li.open').find('.left-arrow').removeClass('open');
-    parent.children('li.open').removeClass("open active");
-    current.children('.left-arrow').addClass("open");
-    li.addClass("open active");
+    parent.children('li.open').removeClass('open active');
+    current.children('.left-arrow').addClass('open');
+    li.addClass('open active');
   }
    
 });
