e4e0633f19cec028f9b114520d8ef6ae6ba1d7d2
Mohammad <mshweiki90@gmail.com>
2017-04-11 10:11:02 +0300

    add views and routes to meteor

--- demo/meteor/client/routes.js
+++ demo/meteor/client/routes.js
@@ -7,7 +7,7 @@ FlowRouter.route('/', {
 });
 
 FlowRouter.route('/charts', {
-  name: 'index',
+  name: 'charts',
   action: () => {
     BlazeLayout.render('mainLayout', { content: 'charts' });
   },
@@ -17,5 +17,54 @@ FlowRouter.route('/tables', {
   name: 'tables',
   action: () => {
     BlazeLayout.render('mainLayout', { content: 'tables' });
+  },
+});
+
+FlowRouter.route('/budget', {
+  name: 'budget',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'budget' });
+  },
+});
+
+FlowRouter.route('/social', {
+  name: 'social',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'social' });
+  },
+});
+
+FlowRouter.route('/data_tables', {
+  name: 'data_tables',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'data_tables' });
+  },
+});
+
+FlowRouter.route('/invoice', {
+  name: 'invoice',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'invoice' });
+  },
+});
+
+FlowRouter.route('/icons', {
+  name: 'icons',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'icons' });
+  },
+});
+
+FlowRouter.route('/colors', {
+  name: 'colors',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'colors' });
+  },
+});
+
+FlowRouter.route('/modals', {
+  name: 'modals',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'modals' });
   },
 });
