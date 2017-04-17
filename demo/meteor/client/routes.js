529fe4d7c2a90b4e76cb97e7b23439407e7d4a02
MohammadShweiki <mshweiki90@gmail.com>
2017-04-17 11:01:04 +0300

    added the formWizard view

--- demo/meteor/client/routes.js
+++ demo/meteor/client/routes.js
@@ -119,3 +119,10 @@ FlowRouter.route('/hover', {
     BlazeLayout.render('mainLayout', { content: 'hover' });
   },
 });
+
+FlowRouter.route('/formWizard', {
+  name: 'formWizard',
+  action: () => {
+    BlazeLayout.render('mainLayout', { content: 'formWizard' });
+  },
+});
