c35955fb15aeb0180b732bbaf771d06e6df349c0
Rayan <ramarat@newrelease.io>
2017-04-23 11:58:27 +0300

    fixed the layout issue and the body styles

--- demo/meteor/client/views/error400.js
+++ demo/meteor/client/views/error400.js
@@ -1,29 +1,15 @@
-// // Template.error400.onRendered(function error400onRendered() {
-// //   // get the current route name (better than checking window.location)
-// //   let routeName = Router.current().route.getName();
+Template.error400.onCreated(function hoverOnCreated() {
 
-// //   // add the class to body if this is the correct route
-// //   if (routeName === 'error400')
-// //     $('body').addClass('error-400');
-// // });
+});
 
-// // // Template.layout.onDestroyed(function error400onDestroyed() {
-// // //   // remove the class to it does not appear on other routes
-// // //   $('body').removeClass('error-body');
-// // // });
+Template.error400.onRendered(function hoveronRendered() {
 
-// Template.error400.onCreated(function error400OnCreated() {
+});
 
-// });
+Template.error400.helpers({
 
-// Template.error400.onRendered(function error400onRendered() {
-  
-// });
+});
 
-// Template.error400.helpers({
+Template.error400.events({
 
-// });
-
-// Template.error400.events({
-
-// });
+});
