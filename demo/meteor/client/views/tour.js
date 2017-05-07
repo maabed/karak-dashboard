ed1205a08c5725989772a4b7c00e89440d56b39b
Rayan <ramarat@newrelease.io>
2017-05-07 10:33:47 +0300

    changed the tabs in the tabs page into a new form

--- demo/meteor/client/views/tour.js
+++ demo/meteor/client/views/tour.js
@@ -144,26 +144,6 @@ Template.tour.onRendered(function touronRendered() {
     });
   });
 
-  $('.statistics-item').hide();
-  let Item = $('.statistics-taps .active a').attr('href');
-  $(Item).show();
-  $('.statistics-taps li').on('click', function() {
-    $('.statistics-taps .active').removeClass('active');
-    $(this).addClass('active');
-    $(Item).fadeOut('100', function() {
-      Item = $('.statistics-taps .active a').attr('href');
-      $(Item).fadeIn('100');
-      let length = $(Item + ' .pitem').length;
-      let x = 8;
-      $(Item + ' .pitem:lt(' + x + ')').show();
-      $(Item + ' > .ploadmore').click(function() {
-        x = (x + 8 <= length) ? x + 8 : length;
-        $(Item + ' .pitem:lt(' + x + ')').show();
-      });
-    });
-    return false;
-  });
-
   formatState = function(state) {
     if (!state.id) {
       return state.text;
