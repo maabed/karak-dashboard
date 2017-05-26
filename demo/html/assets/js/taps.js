05cc5328fe437578cf046c51da9bc644e5e35223
RayanAmaratGT <ramarat@newrelease.io>
2017-05-26 20:55:31 +0300

    Changes on tbas (#141)
    * fixed the tabs in the html version to a new form
    
    * changed the tabs to new form

--- demo/html/assets/js/taps.js
+++ demo/html/assets/js/taps.js
@@ -8,25 +8,6 @@ Copyright 2017 New Release
 $(function($) {
     'use strict';
     $(document).ready(function() {
-        $('.statistics-item').hide();
-        var Item = $('.statistics-taps .active a').attr('href');
-        $(Item).show();
-        $('.statistics-taps li').on('click', function() {
-            $('.statistics-taps .active').removeClass('active');
-            $(this).addClass('active');
-            $(Item).fadeOut('100', function() {
-                Item = $('.statistics-taps .active a').attr('href');
-                $(Item).fadeIn('100');
-                var length = $(Item + ' .pitem').length;
-                var x = 8;
-                $(Item + ' .pitem:lt(' + x + ')').show();
-                $(Item + ' > .ploadmore').click(function() {
-                    x = (x + 8 <= length) ? x + 8 : length;
-                    $(Item + ' .pitem:lt(' + x + ')').show();
-                });
-            });
-            return false;
-        });
         $('.single-tap-details').hide();
         $('.toggle-icon').hide();
         $('.single-tap-title').click(function(e) {
