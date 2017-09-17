89b054d63dcfe3921fbbe69b78bbdf8fbfe60363
Hossam Mourad <ihos4m@gmail.com>
2017-09-17 19:51:43 +0400

    add support for sass in the Grunt watch task

--- demo/html/Gruntfile.js
+++ demo/html/Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          'assets/css/scss_converted.css': 'assets/scss/imports.scss'
+          'assets/css/style.css': 'assets/scss/imports.scss'
         }
       }
     },
@@ -27,6 +27,10 @@ module.exports = function(grunt) {
       less:{
         files:['assets/less/**/*.less'],
         tasks:['less']
+      },
+      sass:{
+        files:['assets/scss/**/*.scss'],
+        tasks:['sass']
       },
     }
   });
