378c00aa1ebe25a89f5ba02920f0bbbbabda3378
Hossam Mourad <ihos4m@gmail.com>
2017-09-19 20:49:45 +0400

    remove nested document.ready function from the notification.js file

--- demo/html/assets/js/notifications.js
+++ demo/html/assets/js/notifications.js
@@ -221,10 +221,7 @@ $(function($) {
 
         $('[data-toggle="tooltip"]').tooltip();
 
-        $(document).ready(function() {
-            $('.select2-init-single').select2();
-        });
-
+        $('.select2-init-single').select2();
     });
 
 });
